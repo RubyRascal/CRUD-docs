a743795d6d75d2540dcbe5fbe777e7273973ec9f
RubyRascal <lekhasorokin2017@yandex.ru>
2021-09-27 17:27:56 +0300

    autoload & validator

--- src/Views/viewUser.php
+++ src/Views/viewUser.php
@@ -4,6 +4,7 @@ class viewUser
 {
     public function ViewFormUser($userData, $errors)
     {
+        // сделать функцию render("принимает имя файла", $массив данных) и сделать класс View extends Singleton
         require 'formUser.php';
     }
 }
